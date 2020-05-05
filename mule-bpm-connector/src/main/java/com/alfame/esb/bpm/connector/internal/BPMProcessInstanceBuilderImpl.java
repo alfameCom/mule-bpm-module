ca6d3d44bec590e671c74475bf3dffa01d8c7ee3
Jani Haglund <jani.haglund@alfame.com>
2020-05-05 14:32:22 +0300

    Improved logging

--- mule-bpm-connector/src/main/java/com/alfame/esb/bpm/connector/internal/BPMProcessInstanceBuilderImpl.java
+++ mule-bpm-connector/src/main/java/com/alfame/esb/bpm/connector/internal/BPMProcessInstanceBuilderImpl.java
@@ -5,8 +5,11 @@ import com.alfame.esb.bpm.api.BPMProcessInstance;
 import com.alfame.esb.bpm.api.BPMProcessInstanceBuilder;
 import com.alfame.esb.bpm.connector.internal.proxies.BPMProcessInstanceProxy;
 import org.flowable.engine.RuntimeService;
+import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.runtime.ProcessInstanceBuilder;
 import org.slf4j.Logger;
+
+import java.util.Map;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -24,30 +27,49 @@ public class BPMProcessInstanceBuilderImpl extends BPMProcessInstanceBuilder {
 
     @Override
     public BPMProcessInstance startProcessInstance() {
+        ProcessInstance processInstance = null;
         ProcessInstanceBuilder instanceBuilder = this.runtimeService.createProcessInstanceBuilder();
+        long startTime = System.currentTimeMillis();
 
-        LOGGER.debug("Starting process instance with definition key: " + this.processDefinitionKey);
+        LOGGER.info(">>>>> process definition {}: instance starting", this.processDefinitionKey);
         instanceBuilder = instanceBuilder.processDefinitionKey(this.processDefinitionKey);
 
         if (this.tenantId != null) {
+            LOGGER.debug(">>>>> process definition {}: instance starting with tenant {}", this.processDefinitionKey, this.tenantId);
             instanceBuilder = instanceBuilder.tenantId(this.tenantId);
         } else {
+            LOGGER.debug(">>>>> process definition {}: instance starting with tenant {}", this.processDefinitionKey, this.engine.getDefaultTenantId());
             instanceBuilder = instanceBuilder.tenantId(this.engine.getDefaultTenantId());
         }
 
         if (this.uniqueBusinessKey != null) {
-            instanceBuilder = instanceBuilder.businessKey(this.tenantId);
+            LOGGER.debug(">>>>> process definition {}: instance starting with business key {}", this.processDefinitionKey, this.uniqueBusinessKey);
+            instanceBuilder = instanceBuilder.businessKey(this.uniqueBusinessKey);
         }
 
         if (this.processInstanceName != null) {
+            LOGGER.debug(">>>>> process definition {}: instance starting with name {}", this.processDefinitionKey, this.processInstanceName);
             instanceBuilder = instanceBuilder.name(this.processInstanceName);
         }
 
         if (this.variables != null) {
+            if (LOGGER.isDebugEnabled()) {
+                for (Map.Entry<String, Object> variable : this.variables.entrySet()) {
+                    LOGGER.debug(">>>>> process definition {}: instance starting with variable {} = {}", this.processDefinitionKey, variable.getKey(), variable.getValue());
+                }
+            }
             instanceBuilder = instanceBuilder.variables(this.variables);
         }
 
-        return new BPMProcessInstanceProxy(instanceBuilder.start());
+        try {
+            processInstance = instanceBuilder.start();
+        } catch (Exception exception) {
+            LOGGER.error("<<<<< process definition {}: instance {} caught exception {} in {} ms", this.processDefinitionKey, processInstance.getProcessInstanceId(), exception, System.currentTimeMillis() - startTime);
+            throw exception;
+        }
+
+        LOGGER.info("<<<<< process definition {}: instance {} started in {} ms", this.processDefinitionKey, processInstance.getProcessInstanceId(), System.currentTimeMillis() - startTime);
+        return new BPMProcessInstanceProxy(processInstance);
     }
 
 }
--- mule-bpm-flowable-activity/src/main/java/org/flowable/mule/MuleSendActivityBehavior.java
+++ mule-bpm-flowable-activity/src/main/java/org/flowable/mule/MuleSendActivityBehavior.java
@@ -15,8 +15,6 @@ package org.flowable.mule;
 import com.alfame.esb.bpm.taskqueue.BPMTaskQueue;
 import com.alfame.esb.bpm.taskqueue.BPMTaskQueueFactory;
 import com.alfame.esb.bpm.taskqueue.BPMTaskResponse;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.flowable.common.engine.api.delegate.Expression;
 import org.flowable.common.engine.impl.context.Context;
 import org.flowable.common.engine.impl.scripting.ScriptingEngines;
@@ -24,11 +22,14 @@ import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.impl.bpmn.behavior.AbstractBpmnActivityBehavior;
 import org.flowable.engine.impl.util.CommandContextUtil;
 import org.flowable.engine.impl.util.Flowable5Util;
+import org.slf4j.Logger;
 
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * <?xml version="1.0" encoding="UTF-8"?>
@@ -64,7 +65,7 @@ import java.util.concurrent.TimeoutException;
  */
 public class MuleSendActivityBehavior extends AbstractBpmnActivityBehavior {
 
-    private static final Log logger = LogFactory.getLog(MuleSendActivityBehavior.class);
+    private static final Logger LOGGER = getLogger(MuleSendActivityBehavior.class);
 
     private static final long serialVersionUID = 1L;
 
@@ -81,12 +82,15 @@ public class MuleSendActivityBehavior extends AbstractBpmnActivityBehavior {
         String payloadExpressionValue = this.getStringFromField(this.payloadExpression, execution);
         String resultVariableValue = this.getStringFromField(this.resultVariable, execution);
         long requestTimeoutValue = Long.parseLong(this.getStringFromField(this.requestTimeout, execution, "300000"));
+        String processDefinitionKey = execution.getProcessDefinitionId().replaceFirst(":.*", "");
+        String processInstanceId = execution.getProcessInstanceId();
+        String currentActivityId = execution.getCurrentActivityId();
         long startTime = System.currentTimeMillis();
 
-        logger.info(">>>>> " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": starting with timeout " + requestTimeoutValue + " ms");
-        logger.trace(">>>>> " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": " + languageValue);
-        logger.trace(">>>>> " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": " + payloadExpressionValue);
-        logger.trace(">>>>> " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": " + execution.getTenantId());
+        LOGGER.info(">>>>> process definition {}: instance {}: activity {}: execution starting with time out {} ms", processDefinitionKey, processInstanceId, currentActivityId, requestTimeoutValue);
+        LOGGER.trace(">>>>> process definition {}: instance {}: activity {}: execution with language {}", processDefinitionKey, processInstanceId, currentActivityId, languageValue);
+        LOGGER.trace(">>>>> process definition {}: instance {}: activity {}: execution with payload expression {}", processDefinitionKey, processInstanceId, currentActivityId, payloadExpressionValue);
+        LOGGER.trace(">>>>> process definition {}: instance {}: activity {}: execution with tenant {}", processDefinitionKey, processInstanceId, currentActivityId, execution.getTenantId());
 
         boolean isFlowable5Execution = false;
         Object attributes = execution;
@@ -99,7 +103,7 @@ public class MuleSendActivityBehavior extends AbstractBpmnActivityBehavior {
 
             } else {
                 ScriptingEngines scriptingEngines = CommandContextUtil.getProcessEngineConfiguration().getScriptingEngines();
-                logger.trace(">>>>> " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": " + scriptingEngines);
+                LOGGER.trace(">>>>> process definition {}: instance {}: activity {}: execution with scripting engined {}", processDefinitionKey, processInstanceId, currentActivityId, scriptingEngines);
                 payload = scriptingEngines.evaluate(payloadExpressionValue, languageValue, execution);
             }
         }
@@ -109,48 +113,54 @@ public class MuleSendActivityBehavior extends AbstractBpmnActivityBehavior {
             if (queue != null) {
                 try {
                     MuleSendActivityTask task = new MuleSendActivityTask(payload, execution.getId(), execution);
-                    logger.trace(">>>>> " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": " + endpointUrlValue);
+                    LOGGER.trace(">>>>> process definition {}: instance {}: activity {}: execution being published to task queue {}", processDefinitionKey, processInstanceId, currentActivityId, endpointUrlValue);
                     queue.publish(task);
                     BPMTaskResponse response = task.waitForResponse(requestTimeoutValue, TimeUnit.MILLISECONDS);
                     if (response == null) {
+                        LOGGER.error("<<<<< process definition {}: instance {}: activity {}: execution ended without response in {} ms", processDefinitionKey, processInstanceId, currentActivityId, System.currentTimeMillis() - startTime);
                         throw new RuntimeException("No response from BPM Queue: " + endpointUrlValue);
                     }
-
-                    logger.trace("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": " + response);
-                    if (resultVariableValue != null && response.getValue() != null) {
-
-                        for (Map.Entry<String, Object> variableToUpdate : response.getVariablesToUpdate().entrySet()) {
-                            logger.debug("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": variable " + variableToUpdate.getKey() + "=" + variableToUpdate.getValue());
-                            execution.setVariable(variableToUpdate.getKey(), variableToUpdate.getValue());
-                        }
-
-                        for (String variableToRemove : response.getVariablesToRemove()) {
-                            logger.debug("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": removing variable " + variableToRemove);
-                            execution.removeVariable(variableToRemove);
-                        }
-
-                        logger.trace("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": variable " + resultVariableValue + "=" + response.getValue());
-                        execution.setVariable(resultVariableValue, response.getValue());
-                    }
                     if (response.getError() != null) {
-                        logger.debug("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId(), response.getError());
+                        LOGGER.error("<<<<< process definition {}: instance {}: activity {}: execution ended with exception {} in {} ms", processDefinitionKey, processInstanceId, currentActivityId, response.getError(), System.currentTimeMillis() - startTime);
                         throw new RuntimeException(response.getError());
                     }
-                } catch (InterruptedException e) {
-                    logger.debug("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId(), e);
-                    throw new RuntimeException(e);
-                } catch (ExecutionException e) {
-                    logger.debug("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId(), e);
-                    throw new RuntimeException(e);
-                } catch (TimeoutException e) {
-                    logger.debug("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId(), e);
-                    throw new RuntimeException(e);
+                    LOGGER.trace("<<<<< process definition {}: instance {}: activity {}: execution ended with response {}", processDefinitionKey, processInstanceId, currentActivityId, response);
+
+                    if (response.getVariablesToUpdate() != null) {
+                        for (Map.Entry<String, Object> variableToUpdate : response.getVariablesToUpdate().entrySet()) {
+                            LOGGER.debug("<<<<< process definition {}: instance {}: activity {}: execution setting variable {} = {}", processDefinitionKey, processInstanceId, currentActivityId, variableToUpdate.getKey(), variableToUpdate.getValue());
+                            execution.setVariable(variableToUpdate.getKey(), variableToUpdate.getValue());
+                        }
+                    }
+
+                    if (response.getVariablesToRemove() != null) {
+                        for (String variableToRemove : response.getVariablesToRemove()) {
+                            LOGGER.debug("<<<<< process definition {}: instance {}: activity {}: execution removing variable {}", processDefinitionKey, processInstanceId, currentActivityId, variableToRemove);
+                            execution.removeVariable(variableToRemove);
+                        }
+                    }
+
+                    if (resultVariableValue != null && response.getValue() != null) {
+                        LOGGER.debug("<<<<< process definition {}: instance {}: activity {}: execution setting variable {} = {}", processDefinitionKey, processInstanceId, currentActivityId, resultVariableValue, response.getValue());
+                        execution.setVariable(resultVariableValue, response.getValue());
+                    }
+                } catch (InterruptedException exception) {
+                    LOGGER.warn("<<<<< process definition {}: instance {}: activity {}: execution was interrupted by exception {} in {} ms", processDefinitionKey, processInstanceId, currentActivityId, exception, System.currentTimeMillis() - startTime);
+                    throw new RuntimeException(exception);
+                } catch (ExecutionException exception) {
+                    LOGGER.error("<<<<< process definition {}: instance {}: activity {}: execution caught exception {} in {} ms", processDefinitionKey, processInstanceId, currentActivityId, exception, System.currentTimeMillis() - startTime);
+                    throw new RuntimeException(exception);
+                } catch (TimeoutException exception) {
+                    LOGGER.warn("<<<<< process definition {}: instance {}: activity {}: execution timed out with exception {} in {} ms", processDefinitionKey, processInstanceId, currentActivityId, exception, System.currentTimeMillis() - startTime);
+                    throw new RuntimeException(exception);
                 }
             } else {
+                LOGGER.error("<<<<< process definition {}: instance {}: activity {}: execution cannot find task queue {} in {} ms", processDefinitionKey, processInstanceId, currentActivityId, endpointUrlValue, System.currentTimeMillis() - startTime);
                 throw new RuntimeException("Cannot find BPM Queue: " + endpointUrlValue);
             }
 
         } else {
+            LOGGER.error("<<<<< process definition {}: instance {}: activity {}: execution cannot find task queue URL in {} ms", processDefinitionKey, processInstanceId, currentActivityId, endpointUrlValue, System.currentTimeMillis() - startTime);
             throw new RuntimeException("Missing endpoint URL, cannot queue Mule Task!");
         }
 
@@ -159,7 +169,7 @@ public class MuleSendActivityBehavior extends AbstractBpmnActivityBehavior {
         } else {
             this.leave(execution);
         }
-        logger.info("<<<<< " + execution.getProcessDefinitionId() + " / " + execution.getCurrentActivityId() + ": " + execution.getProcessInstanceId() + ": done in " + (System.currentTimeMillis() - startTime) + " ms");
+        LOGGER.info("<<<<< process definition {}: instance {}: activity {}: executed in {} ms", processDefinitionKey, processInstanceId, currentActivityId, System.currentTimeMillis() - startTime);
     }
 
     protected String getStringFromField(Expression expression, DelegateExecution execution) {
