263db1055f2a7c22f87ed70c2c766ff7794d53ca
Jani Haglund <jani.haglund@alfame.com>
2022-03-25 14:03:53 +0200

    Implemented tenant aware job retrieval

---
+++ mule-bpm-module/src/main/java/com/alfame/esb/bpm/module/internal/impl/BPMTenantAwareJobDataManager.java
@@ -0,0 +1,83 @@
+package com.alfame.esb.bpm.module.internal.impl;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.flowable.common.engine.impl.Page;
+import org.flowable.job.service.JobServiceConfiguration;
+import org.flowable.job.service.impl.persistence.entity.JobEntity;
+import org.flowable.job.service.impl.persistence.entity.data.impl.MybatisJobDataManager;
+import org.slf4j.Logger;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class BPMTenantAwareJobDataManager extends MybatisJobDataManager {
+
+    private static final Logger LOGGER = getLogger(BPMTenantAwareJobDataManager.class);
+
+    final String tenantId;
+
+    public BPMTenantAwareJobDataManager(JobServiceConfiguration jobServiceConfiguration, String tenantId) {
+        super(jobServiceConfiguration);
+
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    public List<JobEntity> findJobsToExecute(List<String> enabledCategories, Page page) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("jobExecutionScope", jobServiceConfiguration.getJobExecutionScope());
+
+        if (enabledCategories != null && enabledCategories.size() > 0) {
+            params.put("enabledCategories", enabledCategories);
+        }
+
+        params.put("tenantId", tenantId);
+        LOGGER.debug("Finding jobs for tenant {}", tenantId);
+
+        List<JobEntity> jobs = getDbSqlSession().selectList("selectTenantAwareJobsToExecute", params, page);
+        if (LOGGER.isDebugEnabled()) {
+            if (jobs != null) {
+                for (JobEntity job : jobs) {
+                    LOGGER.debug("Found job for tenant {}: {}", tenantId, ReflectionToStringBuilder.toString(job));
+                }
+            }
+        }
+
+        return jobs;
+    }
+
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<JobEntity> findExpiredJobs(List<String> enabledCategories, Page page) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("jobExecutionScope", jobServiceConfiguration.getJobExecutionScope());
+        Date now = jobServiceConfiguration.getClock().getCurrentTime();
+        params.put("now", now);
+        Date maxTimeout = new Date(now.getTime() - jobServiceConfiguration.getAsyncExecutorResetExpiredJobsMaxTimeout());
+        params.put("maxTimeout", maxTimeout);
+
+        if (enabledCategories != null && enabledCategories.size() > 0) {
+            params.put("enabledCategories", enabledCategories);
+        }
+
+        params.put("tenantId", tenantId);
+        LOGGER.debug("Finding expired jobs for tenant {} with max timeout {}", tenantId, maxTimeout);
+
+        List<JobEntity> jobs = getDbSqlSession().selectList("selectTenantAwareExpiredJobs", params, page);
+        if (LOGGER.isDebugEnabled()) {
+            if (jobs != null) {
+                for (JobEntity job : jobs) {
+                    LOGGER.debug("Found expired job for tenant {}: {}", tenantId, ReflectionToStringBuilder.toString(job));
+                }
+            }
+        }
+
+        return jobs;
+    }
+
+}
---
+++ mule-bpm-module/src/main/java/com/alfame/esb/bpm/module/internal/impl/BPMTenantAwareTimerJobDataManager.java
@@ -0,0 +1,84 @@
+package com.alfame.esb.bpm.module.internal.impl;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.flowable.common.engine.impl.Page;
+import org.flowable.job.service.JobServiceConfiguration;
+import org.flowable.job.service.impl.persistence.entity.TimerJobEntity;
+import org.flowable.job.service.impl.persistence.entity.data.impl.MybatisTimerJobDataManager;
+import org.slf4j.Logger;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class BPMTenantAwareTimerJobDataManager extends MybatisTimerJobDataManager {
+
+    private static final Logger LOGGER = getLogger(BPMTenantAwareTimerJobDataManager.class);
+
+    final String tenantId;
+
+    public BPMTenantAwareTimerJobDataManager(JobServiceConfiguration jobServiceConfiguration, String tenantId) {
+        super(jobServiceConfiguration);
+
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<TimerJobEntity> findExpiredJobs(List<String> enabledCategories, Page page) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("jobExecutionScope", jobServiceConfiguration.getJobExecutionScope());
+        Date now = jobServiceConfiguration.getClock().getCurrentTime();
+        params.put("now", now);
+        if (enabledCategories != null && enabledCategories.size() > 0) {
+            params.put("enabledCategories", enabledCategories);
+        }
+
+        params.put("tenantId", tenantId);
+        LOGGER.debug("Finding expired timer jobs for tenant {}", tenantId);
+
+        List<TimerJobEntity> timerJobs = getDbSqlSession().selectList("selectTenantAwareExpiredTimerJobs", params, page);
+        if (LOGGER.isDebugEnabled()) {
+            if (timerJobs != null) {
+                for (TimerJobEntity timerJob : timerJobs) {
+                    LOGGER.debug("Found expired timer job for tenant {}: {}", tenantId, ReflectionToStringBuilder.toString(timerJob));
+                }
+            }
+        }
+
+        return timerJobs;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<TimerJobEntity> findJobsToExecute(List<String> enabledCategories, Page page) {
+        Map<String, Object> params = new HashMap<>(2);
+        String jobExecutionScope = jobServiceConfiguration.getJobExecutionScope();
+        params.put("jobExecutionScope", jobExecutionScope);
+
+        Date now = jobServiceConfiguration.getClock().getCurrentTime();
+        params.put("now", now);
+
+        if (enabledCategories != null && enabledCategories.size() > 0) {
+            params.put("enabledCategories", enabledCategories);
+        }
+
+        params.put("tenantId", tenantId);
+        LOGGER.debug("Finding timer jobs for tenant {}", tenantId);
+
+        List<TimerJobEntity> timerJobs = getDbSqlSession().selectList("selectTenantAwareTimerJobsToExecute", params, page);
+        if (LOGGER.isDebugEnabled()) {
+            if (timerJobs != null) {
+                for (TimerJobEntity timerJob : timerJobs) {
+                    LOGGER.debug("Found timer job for tenant {}: {}", tenantId, ReflectionToStringBuilder.toString(timerJob));
+                }
+            }
+        }
+
+        return timerJobs;
+    }
+
+}
