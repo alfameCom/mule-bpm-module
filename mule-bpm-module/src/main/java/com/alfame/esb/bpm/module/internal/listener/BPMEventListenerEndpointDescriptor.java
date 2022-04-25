29b3979fcfc5f1bef5c8ac0bdebf21166d9de8ff
Juho Hiltunen <juho.hiltunen@alfame.com>
2022-04-25 11:20:32 +0300

    INTG-27: Fix Sonarcloud code smell: Remove this unused import

--- mule-bpm-module/src/main/java/com/alfame/esb/bpm/module/internal/listener/BPMEventListenerEndpointDescriptor.java
+++ mule-bpm-module/src/main/java/com/alfame/esb/bpm/module/internal/listener/BPMEventListenerEndpointDescriptor.java
@@ -4,7 +4,6 @@ import com.alfame.esb.bpm.module.api.config.BPMEventSubscriptionFilter;
 import org.mule.runtime.extension.api.annotation.Alias;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
-import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
